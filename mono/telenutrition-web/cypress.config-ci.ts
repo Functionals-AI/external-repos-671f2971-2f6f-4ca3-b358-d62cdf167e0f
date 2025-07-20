1495122b9209c347ec458e5b287bc595d5fc0979
suducode-functionals <suducode@functionals.ai>
2025-07-20 11:09:09 -0700

    Upload file via API: mono/telenutrition-web/cypress.config-ci.ts

---
+++ mono/telenutrition-web/cypress.config-ci.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  defaultCommandTimeout: 5000,
+  viewportWidth: 1200,
+  viewportHeight: 800,
+  pageLoadTimeout: 100000,
+  requestTimeout: 10000,
+  e2e: {
+    baseUrl: 'http://localhost:3000',
+  },
+  component: {
+    devServer: {
+      framework: 'next',
+      bundler: 'webpack',
+    },
+  },
+});
