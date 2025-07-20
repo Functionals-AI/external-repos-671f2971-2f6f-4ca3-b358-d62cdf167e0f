41e3b74e9bd4325e09fecb54ad0f47d01b1138f7
suducode-functionals <suducode@functionals.ai>
2025-07-20 11:32:12 -0700

    Upload file via API: mono/foodapp/src/index.ts

---
+++ mono/foodapp/src/index.ts
@@ -0,0 +1,15 @@
+import Store from './store'
+import Warehouse from './warehouse'
+import Service from './service'
+import Api from './api'
+import FoodDelivery from './food-delivery'
+import Sync from './sync'
+
+export default {
+  Store,
+  Warehouse,
+  Service,
+  Api,
+  FoodDelivery,
+  Sync
+}
