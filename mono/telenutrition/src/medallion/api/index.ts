a0085c956ca5bb0d4b78caf8e4dd912faa28b63c
suducode-functionals <suducode@functionals.ai>
2025-07-20 11:33:24 -0700

    Upload file via API: mono/telenutrition/src/medallion/api/index.ts

---
+++ mono/telenutrition/src/medallion/api/index.ts
@@ -0,0 +1,12 @@
+import Licenses from './licenses';
+import Providers from './providers';
+import PayerEnrollments from './payer-enrollments';
+import BoardCertificates from './board-certificates'
+
+
+export default {
+  Licenses,
+  Providers,
+  PayerEnrollments,
+  BoardCertificates,
+}
