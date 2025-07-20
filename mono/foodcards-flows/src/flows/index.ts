7efa0a969b792f10bbf3f1b5619aa2ebb9f55135
suducode-functionals <suducode@functionals.ai>
2025-07-20 11:32:02 -0700

    Upload file via API: mono/foodcards-flows/src/flows/index.ts

---
+++ mono/foodcards-flows/src/flows/index.ts
@@ -0,0 +1,7 @@
+import IncommSftpDownload from "./incomm-sftp-download"
+import UcsdEligibilityIngestion from "./ucsd-eligibility-ingestion"
+
+export default {
+  IncommSftpDownload,
+  UcsdEligibilityIngestion,
+}
