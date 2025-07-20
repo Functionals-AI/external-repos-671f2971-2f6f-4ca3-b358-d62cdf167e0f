79ec20d58a99af5b4f4bc063bd5a5dd76eb22708
suducode-functionals <suducode@functionals.ai>
2025-07-20 11:32:42 -0700

    Upload file via API: mono/ops-flows/src/flows/data-ingest-aetna-medicare.ts

---
+++ mono/ops-flows/src/flows/data-ingest-aetna-medicare.ts
@@ -0,0 +1,52 @@
+import { succeed, workflow } from '@mono/common-flows/lib/builder'
+import { AetnaMedicare } from '@mono/ops/lib/data'
+import OpsTasks from '../tasks/ingest'
+
+const SOURCE = 'aetna-medicare'
+const FLOW_NAME = `data-ingest-${SOURCE}`
+
+const MTAG = [ 'ops-flows', 'flows', FLOW_NAME ]
+
+enum State {
+  Ingest = 'Ingest',
+  Success = 'Success',
+}
+
+const SRC_PREFIX = 'aetnamedicare/'
+const FILE_PREFIX='Zipongo.'
+  
+export default workflow(function(config) {
+  if (config.isProduction) {
+    const srcBucket = config.ops_cdk?.sftp?.sftpArchiveBucket?.name
+
+    if (srcBucket) {
+      return {
+        event: {
+          source: ['aws.s3'],
+          detailType: [ 'Object Created' ],
+          detail: {
+            bucket: {
+              name: [ srcBucket ],
+            },
+            object: {
+              key: [ { prefix: `${SRC_PREFIX}${FILE_PREFIX}` } ]
+            }
+          }
+        },
+        startAt: 'Ingest',
+        states: {
+          [State.Ingest]: OpsTasks.ingest({
+            flowName: FLOW_NAME,
+            sourceId: SOURCE,
+            ingestFunction: AetnaMedicare.ingest,
+            next: State.Success
+          }),
+          [State.Success]: succeed(),
+        }
+      }
+    }
+    else {
+      console.log(`Source bucket required.`)
+    }
+  }
+})
