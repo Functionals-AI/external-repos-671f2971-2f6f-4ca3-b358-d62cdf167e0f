a67ef9140121bf1afee61459378c7e68f500f88d
suducode-functionals <suducode@functionals.ai>
2025-07-20 11:35:35 -0700

    Upload file via API: mono/telenutrition/src/candid/api/index.ts

---
+++ mono/telenutrition/src/candid/api/index.ts
@@ -0,0 +1,9 @@
+import organizationProviders from './organization-providers'
+import payers from './payers'
+import contracts from './contracts'
+
+export default {
+  organizationProviders,
+  payers,
+  contracts,
+}
