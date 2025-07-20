d781af1e7fa5bf9a5e632b0bd78e7db7a666b418
suducode-functionals <suducode@functionals.ai>
2025-07-20 11:34:26 -0700

    Upload file via API: mono/telenutrition/src/scheduling/index.ts

---
+++ mono/telenutrition/src/scheduling/index.ts
@@ -0,0 +1,13 @@
+import Appointment from './appointment'
+import Schema from './schema'
+import Service from './service'
+import Sync from './sync'
+import Store from './store'
+
+export default {
+  Appointment,
+  Service,
+  Schema,
+  Sync,
+  Store,
+}
