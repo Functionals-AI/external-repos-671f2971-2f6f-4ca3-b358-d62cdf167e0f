1584f5774b3e0623f9f51ae3f068a9ee0c1e8419
suducode-functionals <suducode@functionals.ai>
2025-07-20 11:18:20 -0700

    Upload file via API: mono/telenutrition-web/src/@/ui-components/tag-input/helpers.ts

---
+++ mono/telenutrition-web/src/@/ui-components/tag-input/helpers.ts
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react';
+import { GroupTagInputOption, TagInputOption } from './types';
+
+export function useDebounce<T>(value: T, delay?: number): T {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setDebouncedValue(value), delay || 500);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
+
+export function transToGroupOption(options: TagInputOption[], groupBy?: string) {
+  if (options.length === 0) {
+    return {};
+  }
+  if (!groupBy) {
+    return {
+      '': options,
+    };
+  }
+
+  const groupOption: GroupTagInputOption = {};
+  options.forEach((option) => {
+    // @ts-ignore
+    const key = (option[groupBy] as string) || '';
+    if (!groupOption[key]) {
+      groupOption[key] = [];
+    }
+    groupOption[key].push(option);
+  });
+  return groupOption;
+}
+
+export function removePickedOption(groupOption: GroupTagInputOption, picked: TagInputOption[]) {
+  const cloneOption = JSON.parse(JSON.stringify(groupOption)) as GroupTagInputOption;
+
+  for (const [key, value] of Object.entries(cloneOption)) {
+    cloneOption[key] = value.filter((val) => !picked.find((p) => p.value === val.value));
+  }
+  return cloneOption;
+}
