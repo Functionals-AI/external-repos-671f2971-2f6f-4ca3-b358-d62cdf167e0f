feea9b0adfdb57d2b2c129a0233deab3b64d052c
suducode-functionals <suducode@functionals.ai>
2025-07-20 11:35:28 -0700

    Upload file via API: mono/telenutrition/src/callcenter/service.ts

---
+++ mono/telenutrition/src/callcenter/service.ts
@@ -0,0 +1,10 @@
+import { Result, err, ok } from 'neverthrow';
+
+import { ErrCode } from '@mono/common/lib/error';
+import { IContext } from '@mono/common/lib/context';
+import Store from './store';
+
+const MTAG = ['telenutrition', 'callcenter', 'service'];
+
+export default {
+};
