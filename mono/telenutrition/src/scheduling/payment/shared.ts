52a0de74c40d039f3879a6a9f4b849fbe9969be5
suducode-functionals <suducode@functionals.ai>
2025-07-20 11:33:42 -0700

    Upload file via API: mono/telenutrition/src/scheduling/payment/shared.ts

---
+++ mono/telenutrition/src/scheduling/payment/shared.ts
@@ -0,0 +1,4 @@
+export type AudioSupport = 'never' | 'optional' | 'default'
+
+export type EligibilityStatus = 'valid' | 'invalid' | 'unchecked' | 'undetermined' | 'expired'
+export type EligibilityCheckType = 'efile' | 'none' | 'snc' | 'invalid'
