72dd21d2c96e7bc0229cd5e5722dd5ec30b17c58
suducode-functionals <suducode@functionals.ai>
2025-07-20 11:17:18 -0700

    Upload file via API: mono/telenutrition-web/src/pages/schedule/auth/coordinator.tsx

---
+++ mono/telenutrition-web/src/pages/schedule/auth/coordinator.tsx
@@ -0,0 +1,5 @@
+import AuthReferrerFlow from '../../../modules/flows/scheduling/flows/auth-referrer-flow';
+
+export default function AuthCoordinatorPage() {
+  return <AuthReferrerFlow flowType="coordinator" />;
+}
