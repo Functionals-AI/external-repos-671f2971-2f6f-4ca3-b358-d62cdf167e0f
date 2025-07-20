5c1f0c8c99fc06f85f41972865656f05afa22433
suducode-functionals <suducode@functionals.ai>
2025-07-20 11:23:48 -0700

    Upload file via API: mono/foodapp-flows/src/flows/index.ts

---
+++ mono/foodapp-flows/src/flows/index.ts
@@ -0,0 +1,13 @@
+import ClientQuartzSendFileToPerformanceKitchen from './client-quartz-send-file-to-performance-kitchen'
+import ClientQuartzSendFileToUmoja from './client-quartz-send-file-to-umoja'
+import ClientQuartzSendReportsToQuartz from './client-quartz-send-reports-to-quartz'
+import EligibilitySyncAccounts from "./eligibility-sync-accounts"
+import WarehouseSync from './warehouse-sync'
+
+export default {
+  ClientQuartzSendFileToPerformanceKitchen,
+  ClientQuartzSendFileToUmoja,
+  ClientQuartzSendReportsToQuartz,
+  EligibilitySyncAccounts,
+  WarehouseSync,
+}
