2c0f4d907fe737d2c96585483ce57f5147a70227
suducode-functionals <suducode@functionals.ai>
2025-07-20 11:16:30 -0700

    Upload file via API: mono/telenutrition-web/src/modules/modal/icons.tsx

---
+++ mono/telenutrition-web/src/modules/modal/icons.tsx
@@ -0,0 +1,5 @@
+import { ExclamationIcon } from '@heroicons/react/solid';
+
+export function ErrorIcon() {
+  return <ExclamationIcon className="h-6 w-6 text-f-light-green" aria-hidden="true" />;
+}
