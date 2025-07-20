a16e4ba8e86ace22a7438897d7eec3a8c34fe18c
suducode-functionals <suducode@functionals.ai>
2025-07-20 11:33:27 -0700

    Upload file via API: mono/telenutrition/src/types/index.ts

---
+++ mono/telenutrition/src/types/index.ts
@@ -0,0 +1,14 @@
+export type * from '../iam/shared';
+export type * from '../rewards/shared';
+export type * from '../scheduling/appointment/types';
+export type * from '../scheduling/benefits/shared';
+export * from '../scheduling/encounter/shared';
+export type * from '../scheduling/flow-v2/types/condition';
+export type * from '../scheduling/flow-v2/types/flow-step-config'
+export type * from '../scheduling/flow-v2/types/flow';
+export type * from '../scheduling/flow-v2/types/widgets';
+export type * from '../scheduling/patient/patient-record';
+export type * from '../scheduling/payment/shared';
+export type * from '../scheduling/provider/shared';
+export type * from '../scheduling/questionnaire/types';
+export type * from './time';
