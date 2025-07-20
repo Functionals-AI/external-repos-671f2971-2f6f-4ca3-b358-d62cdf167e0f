069502b8b3fb2677ace5f91b0423c039a59256dc
suducode-functionals <suducode@functionals.ai>
2025-07-20 11:34:43 -0700

    Upload file via API: mono/telenutrition/src/sticky-notes/index.ts

---
+++ mono/telenutrition/src/sticky-notes/index.ts
@@ -0,0 +1,10 @@
+import Service from './service'
+import Store from './store'
+
+export * as Service from './service'
+export * as Store from './store'
+
+export default {
+    Service,
+    Store,
+}
