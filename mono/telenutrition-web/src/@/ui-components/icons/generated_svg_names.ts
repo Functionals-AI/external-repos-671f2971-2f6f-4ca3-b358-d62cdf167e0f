7141d58d8a915be466be73488984ed6280a56fbc
suducode-functionals <suducode@functionals.ai>
2025-07-20 11:18:15 -0700

    Upload file via API: mono/telenutrition-web/src/@/ui-components/icons/generated_svg_names.ts

---
+++ mono/telenutrition-web/src/@/ui-components/icons/generated_svg_names.ts
@@ -0,0 +1,559 @@
+
+  type A11YCognitiveSvg = 'a11y-cognitive';
+  
+  type A11YDisabilitySvg = 'a11y-disability';
+  
+  type A11YHearingLossSvg = 'a11y-hearing-loss';
+  
+  type A11YVisionLossSvg = 'a11y-vision-loss';
+  
+  type AirplaySvg = 'airplay';
+  
+  type AlertCircleSvg = 'alert-circle';
+  
+  type AlertOctagonSvg = 'alert-octagon';
+  
+  type AlertTriangleSvg = 'alert-triangle';
+  
+  type AlertSvg = 'alert';
+  
+  type AlignCenterSvg = 'align-center';
+  
+  type AlignJustifySvg = 'align-justify';
+  
+  type AlignLeftSvg = 'align-left';
+  
+  type AlignRightSvg = 'align-right';
+  
+  type AnchorSvg = 'anchor';
+  
+  type AperatureSvg = 'aperature';
+  
+  type ArchiveSvg = 'archive';
+  
+  type ArrowDownCircleSvg = 'arrow-down-circle';
+  
+  type ArrowDownLeftSvg = 'arrow-down-left';
+  
+  type ArrowDownRightSvg = 'arrow-down-right';
+  
+  type ArrowDownSvg = 'arrow-down';
+  
+  type ArrowLDownLeftSvg = 'arrow-l-down-left';
+  
+  type ArrowLDownRightSvg = 'arrow-l-down-right';
+  
+  type ArrowLLeftDownSvg = 'arrow-l-left-down';
+  
+  type ArrowLLeftUpSvg = 'arrow-l-left-up';
+  
+  type ArrowLRightDownSvg = 'arrow-l-right-down';
+  
+  type ArrowLRightUpSvg = 'arrow-l-right-up';
+  
+  type ArrowLUpLeftSvg = 'arrow-l-up-left';
+  
+  type ArrowLUpRightSvg = 'arrow-l-up-right';
+  
+  type ArrowLeftCircleSvg = 'arrow-left-circle';
+  
+  type ArrowLeftSvg = 'arrow-left';
+  
+  type ArrowRightCircleSolidSvg = 'arrow-right-circle-solid';
+  
+  type ArrowRightCircleSvg = 'arrow-right-circle';
+  
+  type ArrowRightSvg = 'arrow-right';
+  
+  type ArrowUpCircleSvg = 'arrow-up-circle';
+  
+  type ArrowUpLeftSvg = 'arrow-up-left';
+  
+  type ArrowUpRightSvg = 'arrow-up-right';
+  
+  type ArrowUpSvg = 'arrow-up';
+  
+  type AtSignSvg = 'at-sign';
+  
+  type AwardSvg = 'award';
+  
+  type BarChart2Svg = 'bar-chart-2';
+  
+  type BarChartSvg = 'bar-chart';
+  
+  type BellOffSvg = 'bell-off';
+  
+  type BellSvg = 'bell';
+  
+  type BlankRowSvg = 'blank-row';
+  
+  type BluetoothSvg = 'bluetooth';
+  
+  type BoldSvg = 'bold';
+  
+  type BookOpenSvg = 'book-open';
+  
+  type BookSvg = 'book';
+  
+  type BookmarkSvg = 'bookmark';
+  
+  type BoxSvg = 'box';
+  
+  type BriefcaseSvg = 'briefcase';
+  
+  type BucksDepositSvg = 'bucks-deposit';
+  
+  type BugSvg = 'bug';
+  
+  type CalculatorSvg = 'calculator';
+  
+  type CalendarArrowSvg = 'calendar-arrow';
+  
+  type CalendarDaySvg = 'calendar-day';
+  
+  type CalendarHistorySvg = 'calendar-history';
+  
+  type CalendarWeekSvg = 'calendar-week';
+  
+  type CalendarSvg = 'calendar';
+  
+  type CameraOffSvg = 'camera-off';
+  
+  type CameraSvg = 'camera';
+  
+  type CardStackSvg = 'card-stack';
+  
+  type CashSvg = 'cash';
+  
+  type CastSvg = 'cast';
+  
+  type ChartSvg = 'chart';
+  
+  type CheckCircleSvg = 'check-circle';
+  
+  type CheckSquareSvg = 'check-square';
+  
+  type CheckSvg = 'check';
+  
+  type ChevronDownSvg = 'chevron-down';
+  
+  type ChevronLeftSvg = 'chevron-left';
+  
+  type ChevronRightSvg = 'chevron-right';
+  
+  type ChevronUpSvg = 'chevron-up';
+  
+  type ChevronsDownSvg = 'chevrons-down';
+  
+  type ChevronsLeftSvg = 'chevrons-left';
+  
+  type ChevronsRightSvg = 'chevrons-right';
+  
+  type ChevronsUpSvg = 'chevrons-up';
+  
+  type ChromeSvg = 'chrome';
+  
+  type CircleSvg = 'circle';
+  
+  type ClipboardSvg = 'clipboard';
+  
+  type ClockSvg = 'clock';
+  
+  type CloudRainSvg = 'cloud-rain';
+  
+  type CloudSnowSvg = 'cloud-snow';
+  
+  type CloudSvg = 'cloud';
+  
+  type CodeSvg = 'code';
+  
+  type CoffeeSvg = 'coffee';
+  
+  type ColumnsSvg = 'columns';
+  
+  type CommandSvg = 'command';
+  
+  type CompassSvg = 'compass';
+  
+  type ComponentSvg = 'component';
+  
+  type ContractSvg = 'contract';
+  
+  type CopySvg = 'copy';
+  
+  type CpuSvg = 'cpu';
+  
+  type CreditCardSvg = 'credit-card';
+  
+  type CropSvg = 'crop';
+  
+  type CrosshairSvg = 'crosshair';
+  
+  type DashboardSvg = 'dashboard';
+  
+  type DatabaseSvg = 'database';
+  
+  type DeleteSvg = 'delete';
+  
+  type DiscSvg = 'disc';
+  
+  type DollarSvg = 'dollar';
+  
+  type Download2Svg = 'download-2';
+  
+  type DropSvg = 'drop';
+  
+  type EditSvg = 'edit';
+  
+  type EnvelopeSvg = 'envelope';
+  
+  type ExpandSvg = 'expand';
+  
+  type ExternalLinkSvg = 'external-link';
+  
+  type EyeOffSvg = 'eye-off';
+  
+  type EyeSvg = 'eye';
+  
+  type FaceFrownSvg = 'face-frown';
+  
+  type FaceMehSvg = 'face-meh';
+  
+  type FaceSmileSvg = 'face-smile';
+  
+  type FastForwardSvg = 'fast-forward';
+  
+  type FeatherSvg = 'feather';
+  
+  type FeedbackSvg = 'feedback';
+  
+  type FileMinusSvg = 'file-minus';
+  
+  type FilePlusSvg = 'file-plus';
+  
+  type FileTextSvg = 'file-text';
+  
+  type FileSvg = 'file';
+  
+  type FilmSvg = 'film';
+  
+  type FilterSvg = 'filter';
+  
+  type FlagFilledSvg = 'flag---filled';
+  
+  type FlagSvg = 'flag';
+  
+  type FolderMinusSvg = 'folder-minus';
+  
+  type FolderPlusSvg = 'folder-plus';
+  
+  type FolderSvg = 'folder';
+  
+  type FoodStampSvg = 'food-stamp';
+  
+  type FoodSvg = 'food';
+  
+  type GiftSvg = 'gift';
+  
+  type GlobeSvg = 'globe';
+  
+  type GridSvg = 'grid';
+  
+  type HamburgerSvg = 'hamburger';
+  
+  type HardDriveSvg = 'hard-drive';
+  
+  type HeadphonesSvg = 'headphones';
+  
+  type HeartFilledSvg = 'heart-filled';
+  
+  type HeartSvg = 'heart';
+  
+  type HelpCircleSvg = 'help-circle';
+  
+  type HexagonSvg = 'hexagon';
+  
+  type HistorySvg = 'history';
+  
+  type HomeSvg = 'home';
+  
+  type HouseholdSvg = 'household';
+  
+  type ImageSvg = 'image';
+  
+  type ImmediateSvg = 'immediate';
+  
+  type InboxSvg = 'inbox';
+  
+  type InfoCircleSvg = 'info-circle';
+  
+  type ItalicSvg = 'italic';
+  
+  type KebabSvg = 'kebab';
+  
+  type KeySvg = 'key';
+  
+  type LaptopSvg = 'laptop';
+  
+  type LayersSvg = 'layers';
+  
+  type LayoutSvg = 'layout';
+  
+  type LifeRingSvg = 'life-ring';
+  
+  type LinkSvg = 'link';
+  
+  type ListSvg = 'list';
+  
+  type LoaderSvg = 'loader';
+  
+  type LockSvg = 'lock';
+  
+  type LogInSvg = 'log-in';
+  
+  type LogOutSvg = 'log-out';
+  
+  type MapPinSvg = 'map-pin';
+  
+  type MapSvg = 'map';
+  
+  type MeatballsSvg = 'meatballs';
+  
+  type MessageSquareSvg = 'message-square';
+  
+  type MicrophoneOffSvg = 'microphone-off';
+  
+  type MicrophoneSvg = 'microphone';
+  
+  type MinimizeSvg = 'minimize';
+  
+  type MinusCircleSvg = 'minus-circle';
+  
+  type MinusSquareSvg = 'minus-square';
+  
+  type MinusSvg = 'minus';
+  
+  type MonitorSvg = 'monitor';
+  
+  type MoonSvg = 'moon';
+  
+  type MusicSvg = 'music';
+  
+  type NavigationSvg = 'navigation';
+  
+  type NoSvg = 'no';
+  
+  type NumberSpinnerSvg = 'number-spinner';
+  
+  type OctagonSvg = 'octagon';
+  
+  type PackageSvg = 'package';
+  
+  type PaperclipSvg = 'paperclip';
+  
+  type PatientCardSvg = 'patient-card';
+  
+  type PatientSvg = 'patient';
+  
+  type PauseCircleSvg = 'pause-circle';
+  
+  type PauseSvg = 'pause';
+  
+  type PenSvg = 'pen';
+  
+  type PerPlateSvg = 'per-plate';
+  
+  type PhoneSvg = 'phone';
+  
+  type PlaceholderSvg = 'placeholder';
+  
+  type PlayCircleSvg = 'play-circle';
+  
+  type PlaySvg = 'play';
+  
+  type PlusCircleSvg = 'plus-circle';
+  
+  type PlusSquareSvg = 'plus-square';
+  
+  type PlusSvg = 'plus';
+  
+  type PrinterSvg = 'printer';
+  
+  type PulseSvg = 'pulse';
+  
+  type QuestionCircleSvg = 'question-circle';
+  
+  type RadioSvg = 'radio';
+  
+  type RedoSvg = 'redo';
+  
+  type RefreshCcwSvg = 'refresh-ccw';
+  
+  type RefreshCwSvg = 'refresh-cw';
+  
+  type RepeatSvg = 'repeat';
+  
+  type ReportingDashboardSvg = 'reporting-dashboard';
+  
+  type RewindSvg = 'rewind';
+  
+  type RssSvg = 'rss';
+  
+  type SaveSvg = 'save';
+  
+  type ScissorsSvg = 'scissors';
+  
+  type SearchSvg = 'search';
+  
+  type SelectionSvg = 'selection';
+  
+  type SendSvg = 'send';
+  
+  type ServerSvg = 'server';
+  
+  type SettingsSvg = 'settings';
+  
+  type Share2Svg = 'share-2';
+  
+  type ShareSvg = 'share';
+  
+  type ShieldOffSvg = 'shield-off';
+  
+  type ShieldSvg = 'shield';
+  
+  type ShoppingBagSvg = 'shopping-bag';
+  
+  type ShoppingCartSvg = 'shopping-cart';
+  
+  type ShuffleSvg = 'shuffle';
+  
+  type SidebarSvg = 'sidebar';
+  
+  type SkipBackSvg = 'skip-back';
+  
+  type SkipForwardSvg = 'skip-forward';
+  
+  type SlidersSvg = 'sliders';
+  
+  type SmartphoneSvg = 'smartphone';
+  
+  type SortSvg = 'sort';
+  
+  type SpeakerSvg = 'speaker';
+  
+  type SquareSvg = 'square';
+  
+  type StarSvg = 'star';
+  
+  type StethoscopeSvg = 'stethoscope';
+  
+  type StickyNoteFilledSvg = 'sticky-note-filled';
+  
+  type StickyNotePlusSvg = 'sticky-note-plus';
+  
+  type StickyNoteSvg = 'sticky-note';
+  
+  type StopCircleSvg = 'stop-circle';
+  
+  type SunSvg = 'sun';
+  
+  type SunriseSvg = 'sunrise';
+  
+  type SunsetSvg = 'sunset';
+  
+  type TabletSvg = 'tablet';
+  
+  type TagSvg = 'tag';
+  
+  type TargetSvg = 'target';
+  
+  type TerminalSvg = 'terminal';
+  
+  type ThermometerSvg = 'thermometer';
+  
+  type ThumbsDownSvg = 'thumbs-down';
+  
+  type ThumbsUpSvg = 'thumbs-up';
+  
+  type Trash2Svg = 'trash-2';
+  
+  type TrashSvg = 'trash';
+  
+  type TrendingDownSvg = 'trending-down';
+  
+  type TrendingUpSvg = 'trending-up';
+  
+  type TruckSvg = 'truck';
+  
+  type TvSvg = 'tv';
+  
+  type TypeSvg = 'type';
+  
+  type UmbrellaSvg = 'umbrella';
+  
+  type UnderlineSvg = 'underline';
+  
+  type UndoSvg = 'undo';
+  
+  type UnlockSvg = 'unlock';
+  
+  type Upload2Svg = 'upload-2';
+  
+  type UploadSvg = 'upload';
+  
+  type UserCheckSvg = 'user-check';
+  
+  type UserMinusSvg = 'user-minus';
+  
+  type UserPlusSvg = 'user-plus';
+  
+  type UserXSvg = 'user-x';
+  
+  type UserSvg = 'user';
+  
+  type UsersSvg = 'users';
+  
+  type VacationSvg = 'vacation';
+  
+  type VideoOffSvg = 'video-off';
+  
+  type VideoSvg = 'video';
+  
+  type VoicemailSvg = 'voicemail';
+  
+  type VolumeHighSvg = 'volume-high';
+  
+  type VolumeLowSvg = 'volume-low';
+  
+  type VolumeXSvg = 'volume-x';
+  
+  type VolumeSvg = 'volume';
+  
+  type WalletSvg = 'wallet';
+  
+  type WifiOffSvg = 'wifi-off';
+  
+  type WifiSvg = 'wifi';
+  
+  type WindSvg = 'wind';
+  
+  type WrenchSvg = 'wrench';
+  
+  type XCircleSvg = 'x-circle';
+  
+  type XOctagonSvg = 'x-octagon';
+  
+  type XSquareSvg = 'x-square';
+  
+  type XSvg = 'x';
+  
+  type ZapOffSvg = 'zap-off';
+  
+  type ZapSvg = 'zap';
+  
+  type ZoomInSvg = 'zoom-in';
+  
+  type ZoomOutSvg = 'zoom-out';
+  
+export type SvgName = A11YCognitiveSvg | A11YDisabilitySvg | A11YHearingLossSvg | A11YVisionLossSvg | AirplaySvg | AlertCircleSvg | AlertOctagonSvg | AlertTriangleSvg | AlertSvg | AlignCenterSvg | AlignJustifySvg | AlignLeftSvg | AlignRightSvg | AnchorSvg | AperatureSvg | ArchiveSvg | ArrowDownCircleSvg | ArrowDownLeftSvg | ArrowDownRightSvg | ArrowDownSvg | ArrowLDownLeftSvg | ArrowLDownRightSvg | ArrowLLeftDownSvg | ArrowLLeftUpSvg | ArrowLRightDownSvg | ArrowLRightUpSvg | ArrowLUpLeftSvg | ArrowLUpRightSvg | ArrowLeftCircleSvg | ArrowLeftSvg | ArrowRightCircleSolidSvg | ArrowRightCircleSvg | ArrowRightSvg | ArrowUpCircleSvg | ArrowUpLeftSvg | ArrowUpRightSvg | ArrowUpSvg | AtSignSvg | AwardSvg | BarChart2Svg | BarChartSvg | BellOffSvg | BellSvg | BlankRowSvg | BluetoothSvg | BoldSvg | BookOpenSvg | BookSvg | BookmarkSvg | BoxSvg | BriefcaseSvg | BucksDepositSvg | BugSvg | CalculatorSvg | CalendarArrowSvg | CalendarDaySvg | CalendarHistorySvg | CalendarWeekSvg | CalendarSvg | CameraOffSvg | CameraSvg | CardStackSvg | CashSvg | CastSvg | ChartSvg | CheckCircleSvg | CheckSquareSvg | CheckSvg | ChevronDownSvg | ChevronLeftSvg | ChevronRightSvg | ChevronUpSvg | ChevronsDownSvg | ChevronsLeftSvg | ChevronsRightSvg | ChevronsUpSvg | ChromeSvg | CircleSvg | ClipboardSvg | ClockSvg | CloudRainSvg | CloudSnowSvg | CloudSvg | CodeSvg | CoffeeSvg | ColumnsSvg | CommandSvg | CompassSvg | ComponentSvg | ContractSvg | CopySvg | CpuSvg | CreditCardSvg | CropSvg | CrosshairSvg | DashboardSvg | DatabaseSvg | DeleteSvg | DiscSvg | DollarSvg | Download2Svg | DropSvg | EditSvg | EnvelopeSvg | ExpandSvg | ExternalLinkSvg | EyeOffSvg | EyeSvg | FaceFrownSvg | FaceMehSvg | FaceSmileSvg | FastForwardSvg | FeatherSvg | FeedbackSvg | FileMinusSvg | FilePlusSvg | FileTextSvg | FileSvg | FilmSvg | FilterSvg | FlagFilledSvg | FlagSvg | FolderMinusSvg | FolderPlusSvg | FolderSvg | FoodStampSvg | FoodSvg | GiftSvg | GlobeSvg | GridSvg | HamburgerSvg | HardDriveSvg | HeadphonesSvg | HeartFilledSvg | HeartSvg | HelpCircleSvg | HexagonSvg | HistorySvg | HomeSvg | HouseholdSvg | ImageSvg | ImmediateSvg | InboxSvg | InfoCircleSvg | ItalicSvg | KebabSvg | KeySvg | LaptopSvg | LayersSvg | LayoutSvg | LifeRingSvg | LinkSvg | ListSvg | LoaderSvg | LockSvg | LogInSvg | LogOutSvg | MapPinSvg | MapSvg | MeatballsSvg | MessageSquareSvg | MicrophoneOffSvg | MicrophoneSvg | MinimizeSvg | MinusCircleSvg | MinusSquareSvg | MinusSvg | MonitorSvg | MoonSvg | MusicSvg | NavigationSvg | NoSvg | NumberSpinnerSvg | OctagonSvg | PackageSvg | PaperclipSvg | PatientCardSvg | PatientSvg | PauseCircleSvg | PauseSvg | PenSvg | PerPlateSvg | PhoneSvg | PlaceholderSvg | PlayCircleSvg | PlaySvg | PlusCircleSvg | PlusSquareSvg | PlusSvg | PrinterSvg | PulseSvg | QuestionCircleSvg | RadioSvg | RedoSvg | RefreshCcwSvg | RefreshCwSvg | RepeatSvg | ReportingDashboardSvg | RewindSvg | RssSvg | SaveSvg | ScissorsSvg | SearchSvg | SelectionSvg | SendSvg | ServerSvg | SettingsSvg | Share2Svg | ShareSvg | ShieldOffSvg | ShieldSvg | ShoppingBagSvg | ShoppingCartSvg | ShuffleSvg | SidebarSvg | SkipBackSvg | SkipForwardSvg | SlidersSvg | SmartphoneSvg | SortSvg | SpeakerSvg | SquareSvg | StarSvg | StethoscopeSvg | StickyNoteFilledSvg | StickyNotePlusSvg | StickyNoteSvg | StopCircleSvg | SunSvg | SunriseSvg | SunsetSvg | TabletSvg | TagSvg | TargetSvg | TerminalSvg | ThermometerSvg | ThumbsDownSvg | ThumbsUpSvg | Trash2Svg | TrashSvg | TrendingDownSvg | TrendingUpSvg | TruckSvg | TvSvg | TypeSvg | UmbrellaSvg | UnderlineSvg | UndoSvg | UnlockSvg | Upload2Svg | UploadSvg | UserCheckSvg | UserMinusSvg | UserPlusSvg | UserXSvg | UserSvg | UsersSvg | VacationSvg | VideoOffSvg | VideoSvg | VoicemailSvg | VolumeHighSvg | VolumeLowSvg | VolumeXSvg | VolumeSvg | WalletSvg | WifiOffSvg | WifiSvg | WindSvg | WrenchSvg | XCircleSvg | XOctagonSvg | XSquareSvg | XSvg | ZapOffSvg | ZapSvg | ZoomInSvg | ZoomOutSvg;
+export const svgs: SvgName[] = ['a11y-cognitive','a11y-disability','a11y-hearing-loss','a11y-vision-loss','airplay','alert-circle','alert-octagon','alert-triangle','alert','align-center','align-justify','align-left','align-right','anchor','aperature','archive','arrow-down-circle','arrow-down-left','arrow-down-right','arrow-down','arrow-l-down-left','arrow-l-down-right','arrow-l-left-down','arrow-l-left-up','arrow-l-right-down','arrow-l-right-up','arrow-l-up-left','arrow-l-up-right','arrow-left-circle','arrow-left','arrow-right-circle-solid','arrow-right-circle','arrow-right','arrow-up-circle','arrow-up-left','arrow-up-right','arrow-up','at-sign','award','bar-chart-2','bar-chart','bell-off','bell','blank-row','bluetooth','bold','book-open','book','bookmark','box','briefcase','bucks-deposit','bug','calculator','calendar-arrow','calendar-day','calendar-history','calendar-week','calendar','camera-off','camera','card-stack','cash','cast','chart','check-circle','check-square','check','chevron-down','chevron-left','chevron-right','chevron-up','chevrons-down','chevrons-left','chevrons-right','chevrons-up','chrome','circle','clipboard','clock','cloud-rain','cloud-snow','cloud','code','coffee','columns','command','compass','component','contract','copy','cpu','credit-card','crop','crosshair','dashboard','database','delete','disc','dollar','download-2','drop','edit','envelope','expand','external-link','eye-off','eye','face-frown','face-meh','face-smile','fast-forward','feather','feedback','file-minus','file-plus','file-text','file','film','filter','flag---filled','flag','folder-minus','folder-plus','folder','food-stamp','food','gift','globe','grid','hamburger','hard-drive','headphones','heart-filled','heart','help-circle','hexagon','history','home','household','image','immediate','inbox','info-circle','italic','kebab','key','laptop','layers','layout','life-ring','link','list','loader','lock','log-in','log-out','map-pin','map','meatballs','message-square','microphone-off','microphone','minimize','minus-circle','minus-square','minus','monitor','moon','music','navigation','no','number-spinner','octagon','package','paperclip','patient-card','patient','pause-circle','pause','pen','per-plate','phone','placeholder','play-circle','play','plus-circle','plus-square','plus','printer','pulse','question-circle','radio','redo','refresh-ccw','refresh-cw','repeat','reporting-dashboard','rewind','rss','save','scissors','search','selection','send','server','settings','share-2','share','shield-off','shield','shopping-bag','shopping-cart','shuffle','sidebar','skip-back','skip-forward','sliders','smartphone','sort','speaker','square','star','stethoscope','sticky-note-filled','sticky-note-plus','sticky-note','stop-circle','sun','sunrise','sunset','tablet','tag','target','terminal','thermometer','thumbs-down','thumbs-up','trash-2','trash','trending-down','trending-up','truck','tv','type','umbrella','underline','undo','unlock','upload-2','upload','user-check','user-minus','user-plus','user-x','user','users','vacation','video-off','video','voicemail','volume-high','volume-low','volume-x','volume','wallet','wifi-off','wifi','wind','wrench','x-circle','x-octagon','x-square','x','zap-off','zap','zoom-in','zoom-out'];
