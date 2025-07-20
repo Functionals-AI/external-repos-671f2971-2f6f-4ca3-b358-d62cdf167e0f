83d3713a316f38fd29f38d082bafaf71d7486380
suducode-functionals <suducode@functionals.ai>
2025-07-20 11:26:17 -0700

    Upload file via API: mono/telenutrition-flows/src/flows/index.ts

---
+++ mono/telenutrition-flows/src/flows/index.ts
@@ -0,0 +1,43 @@
+import ActivitiesAppointmentCompleted from './activities-appointment-completed'
+import AllocateScheduleSlots from './allocate-schedule-slots'
+import BillingEtl from './billing-etl'
+import CandidSyncBillingTransactions from './candid-sync-billing-transactions'
+import CandidSyncProviders from './candid-sync-providers'
+import CioCreateSegment from './cio-create-segment'
+import CioSyncCancelledAppointments from './cio-sync-cancelled-appointments'
+import CioSyncCustomers from './cio-sync-customers'
+import CioSyncSegments from './cio-sync-segments'
+import EventsVpAppointment from './events-vp-appointment'
+import IncentivesGrantRewards from './incentives-grant-rewards'
+import MedallionSync from './medallion-sync'
+import OverbookingAssignSlots from './overbooking-assign-slots'
+import PaymentEligibilityChecks from './payment-eligibility-checks'
+import ReferralLifecycleProcess from './referral-lifecycle-process'
+import RemapZoomLinks from './remap-zoom-links'
+import SchedulingSyncProviders from './scheduling-sync-providers'
+import SyncMeetingParticipants from './sync-meeting-participants'
+import UpsertIdentities from './upsert-identities'
+import WorkrampSync from './workramp-sync'
+
+export default {
+  ActivitiesAppointmentCompleted,
+  AllocateScheduleSlots,
+  BillingEtl,
+  CandidSyncBillingTransactions,
+  CandidSyncProviders,
+  CioCreateSegment,
+  CioSyncCancelledAppointments,
+  CioSyncCustomers,
+  CioSyncSegments,
+  EventsVpAppointment,
+  IncentivesGrantRewards,
+  MedallionSync,
+  OverbookingAssignSlots,
+  PaymentEligibilityChecks,
+  ReferralLifecycleProcess,
+  RemapZoomLinks,
+  SchedulingSyncProviders,
+  SyncMeetingParticipants,
+  UpsertIdentities,
+  WorkrampSync,
+}
