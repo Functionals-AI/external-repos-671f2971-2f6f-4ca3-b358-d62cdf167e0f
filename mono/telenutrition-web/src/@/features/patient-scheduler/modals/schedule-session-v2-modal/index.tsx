437da165226d672abc2fa1ed023a197e1cf4ec4b
suducode-functionals <suducode@functionals.ai>
2025-07-20 11:18:34 -0700

    Upload file via API: mono/telenutrition-web/src/@/features/patient-scheduler/modals/schedule-session-v2-modal/index.tsx

---
+++ mono/telenutrition-web/src/@/features/patient-scheduler/modals/schedule-session-v2-modal/index.tsx
@@ -0,0 +1,176 @@
+import React from 'react';
+
+import { Trans, useTranslation } from 'react-i18next';
+import Modal from '@/modules/modal/ui/modal';
+import type { RadioGroupTieredQuestion } from '@mono/telenutrition/lib/types';
+import Section from '@/ui-components/section';
+import DataDisplay from '@/ui-components/data-display';
+import { FormV2, useForm } from '@/modules/form/form';
+import { useModal } from '@/modules/modal';
+import useMemberHelpers from '@/modules/member/useMemberHelpers';
+import RadioGroupV2 from '@/modules/form/radio-group-v2';
+import { ScheduleSessionModalV2Data } from '@/modules/modal/types';
+import { HouseholdMemberSchedulable } from 'api/types';
+import FormItem from '@/modules/form/form-item';
+import AsyncPatientSearchFormField from '@/modules/modal/modals/schedule-session/steps/async-patient-search';
+
+enum ScheduleOption {
+  SELF = 'SELF',
+  OTHER = 'OTHER',
+}
+
+enum ScheduleOtherSubOption {
+  KNOWN_PROVIDER = 'KNOWN_PROVIDER',
+  ANY_OTHER_PROVIDER = 'ANY_OTHER_PROVIDER',
+}
+
+export const scheduleWith: RadioGroupTieredQuestion = {
+  type: 'input:radio',
+  key: 'nestedRadioWithOther',
+  label: 'Schedule with self or other provider',
+  options: [
+    {
+      type: 'basic',
+      label: 'Myself',
+      value: ScheduleOption.SELF,
+    },
+    {
+      type: 'basic',
+      label: 'A different provider',
+      value: ScheduleOption.OTHER,
+    },
+  ],
+  required: true,
+};
+
+interface ScheduleSessionFormFields {
+  scheduleOption: ScheduleOption;
+  scheduleOtherSubOption: ScheduleOtherSubOption;
+  patient: HouseholdMemberSchedulable;
+}
+
+export default function ScheduleSessionV2Modal({ patient }: ScheduleSessionModalV2Data) {
+  const modal = useModal();
+  const { t } = useTranslation();
+  const form = useForm<ScheduleSessionFormFields>({
+    defaultValues: {
+      scheduleOption: ScheduleOption.SELF,
+      patient,
+    },
+  });
+  const memberHelpers = useMemberHelpers();
+
+  const scheduleOptionWatch = form.watch('scheduleOption');
+
+  function handleSubmit(values: ScheduleSessionFormFields) {
+    const { patient } = values;
+    if (values.scheduleOption === ScheduleOption.SELF) {
+      modal.openPrimary({
+        type: 'schedule-visit-self',
+        patient,
+      });
+    } else if (values.scheduleOption === ScheduleOption.OTHER) {
+      if (!values.scheduleOtherSubOption) {
+        form.setError('scheduleOtherSubOption', { message: t('Required') });
+        return;
+      }
+      if (values.scheduleOtherSubOption === ScheduleOtherSubOption.KNOWN_PROVIDER) {
+        modal.openPrimary({
+          type: 'schedule-with-other-known',
+          patient,
+        });
+      } else if (values.scheduleOtherSubOption === ScheduleOtherSubOption.ANY_OTHER_PROVIDER) {
+        modal.openPrimary({
+          type: 'schedule-with-other-unknown',
+          patient,
+        });
+      }
+    }
+  }
+
+  return (
+    <Modal size="lg" panelClassName="overflow-visible">
+      <FormV2 form={form} onSubmit={handleSubmit}>
+        <Modal.Header title={<Trans>Scheduling options</Trans>} />
+        <Modal.Body className="overflow-visible">
+          <Section title={t('Member')}>
+            {patient ? (
+              <DataDisplay
+                label={t('Member')}
+                content={<div>{memberHelpers.getDisplayNameForPatient(patient).value}</div>}
+              />
+            ) : (
+              <FormItem
+                className="max-w-[20rem]"
+                id="patient"
+                form={form}
+                label={<Trans>Member ID or Name (Format: Last, First)</Trans>}
+                rules={{ required: true }}
+                renderItem={(field) => (
+                  <AsyncPatientSearchFormField
+                    value={field.value as HouseholdMemberSchedulable}
+                    patientCanScheduleValidator={(patient) => {
+                      if (!patient.schedulingInfo.canSchedule) {
+                        return { canSchedule: false, reasons: patient.schedulingInfo.errors };
+                      }
+
+                      return { canSchedule: true };
+                    }}
+                    onChange={(option) => {
+                      field.onChange(option?.value ?? null);
+                    }}
+                  />
+                )}
+              />
+            )}
+          </Section>
+          <Section.Divider />
+          <Section title={t('Options')}>
+            <div>
+              <RadioGroupV2
+                form={form}
+                id="scheduleOption"
+                label={<Trans>Schedule with self or other provider</Trans>}
+                options={[
+                  {
+                    label: t('Myself'),
+                    value: ScheduleOption.SELF,
+                  },
+                  {
+                    label: t('A different provider'),
+                    value: ScheduleOption.OTHER,
+                  },
+                ]}
+                rules={{ required: true }}
+              />
+              {scheduleOptionWatch === ScheduleOption.OTHER && (
+                <div className="ml-4 mt-2">
+                  <RadioGroupV2
+                    form={form}
+                    id="scheduleOtherSubOption"
+                    options={[
+                      {
+                        label: t('I know what provider I want to schedule with'),
+                        value: ScheduleOtherSubOption.KNOWN_PROVIDER,
+                      },
+                      {
+                        label: t('I want to search for a provider based on availability'),
+                        value: ScheduleOtherSubOption.ANY_OTHER_PROVIDER,
+                      },
+                    ]}
+                  />
+                </div>
+              )}
+            </div>
+          </Section>
+        </Modal.Body>
+        <Modal.Footer className="justify-end">
+          <Modal.Footer.ButtonGroup>
+            <Modal.Footer.SecondaryCloseButton>{t('Cancel')}</Modal.Footer.SecondaryCloseButton>
+            <Modal.Footer.PrimaryButton>{t('Next')}</Modal.Footer.PrimaryButton>
+          </Modal.Footer.ButtonGroup>
+        </Modal.Footer>
+      </FormV2>
+    </Modal>
+  );
+}
