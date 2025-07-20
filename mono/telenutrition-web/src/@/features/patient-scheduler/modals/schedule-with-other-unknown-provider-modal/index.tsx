979d6b020ac7efb8520c4f56f18c716ce817b0c5
suducode-functionals <suducode@functionals.ai>
2025-07-20 11:18:38 -0700

    Upload file via API: mono/telenutrition-web/src/@/features/patient-scheduler/modals/schedule-with-other-unknown-provider-modal/index.tsx

---
+++ mono/telenutrition-web/src/@/features/patient-scheduler/modals/schedule-with-other-unknown-provider-modal/index.tsx
@@ -0,0 +1,31 @@
+import Modal from '@/modules/modal/ui/modal';
+import { HouseholdMemberSchedulable } from 'api/types';
+import type { PatientRecord, ProviderRecordShort } from '@mono/telenutrition/lib/types';
+import { SessionDuration, SessionType } from 'types/globals';
+
+import ScheduleWithOtherUnknownProviderFlow from '../../flows/other-unknown/schedule';
+import { AppointmentData } from 'api/useGetAppointments';
+
+export interface ScheduleWithOtherUnknownProviderFormFields {
+  patient: HouseholdMemberSchedulable;
+  duration: SessionDuration;
+  sessionType: SessionType;
+  date: string;
+  selectedAppointmentData: AppointmentData;
+  selectedProvider: ProviderRecordShort;
+}
+
+type ScheduleWithOtherUnknownProviderModalProps = {
+  patient: PatientRecord;
+};
+
+export default function ScheduleWithOtherUnknownProviderModal({
+  patient,
+}: ScheduleWithOtherUnknownProviderModalProps) {
+
+  return (
+    <Modal size="lg">
+      <ScheduleWithOtherUnknownProviderFlow patient={patient} />
+    </Modal>
+  );
+}
