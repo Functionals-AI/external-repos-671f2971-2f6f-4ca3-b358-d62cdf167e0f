b43032e1ef83cfc2be7a9d529934c3e55602b9bc
suducode-functionals <suducode@functionals.ai>
2025-07-20 11:24:46 -0700

    Upload file via API: mono/marketing-flows/src/runner.ts

---
+++ mono/marketing-flows/src/runner.ts
@@ -0,0 +1,46 @@
+import {Context, Logger} from '@mono/common'
+import { IContext } from '@mono/common/lib/context'
+import { runTask } from '@mono/common-flows/lib/runner'
+
+import flows from './flows'
+
+const MTAG = Logger.tag()
+
+function signals(context: IContext) {
+  process.on('SIGTERM', async () => {
+    terminate(context)
+  })
+}
+
+async function terminate(context: IContext) {
+  await Context.destroy(context)
+  process.exit()
+}
+
+async function main() {
+  const TAG = [...MTAG, 'main']
+  const flowId = process.argv[2]
+  const taskId = process.argv[3]
+  const context = await Context.create()
+  const {logger} = context
+  
+  try {
+    signals(context)
+    
+    const result = await runTask(context, flows, flowId, taskId)
+    
+    if (result.isErr()) {
+      process.exitCode = 1
+    } 
+  } catch(e) {
+    logger.exception(context, TAG, e)
+    process.exitCode = 1
+  } finally {
+    await terminate(context)
+  }
+}
+
+main().catch((err) => {
+  console.error(err)
+  process.exit(1)
+})
