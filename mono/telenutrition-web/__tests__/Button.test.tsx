b6726c7f1123ddcc0e29526a1ac798397d9567b4
suducode-functionals <suducode@functionals.ai>
2025-07-20 11:12:46 -0700

    Upload file via API: mono/telenutrition-web/__tests__/Button.test.tsx

---
+++ mono/telenutrition-web/__tests__/Button.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import Button from '../src/components/button';
+
+describe('Button', () => {
+  it('renders', () => {
+    // expect(true).to.equal(true)
+  });
+});
