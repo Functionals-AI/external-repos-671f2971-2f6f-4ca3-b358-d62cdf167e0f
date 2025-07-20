acdd8348936d43dadc317f9561a38bda099d7d65
suducode-functionals <suducode@functionals.ai>
2025-07-20 11:37:49 -0700

    Upload file via API: mono/ops/src/data/sources/careoregon.ts

---
+++ mono/ops/src/data/sources/careoregon.ts
@@ -0,0 +1,47 @@
+import { err, ok, Result } from "neverthrow";
+
+import { IContext } from '@mono/common/lib/context'
+import { ErrCode } from "@mono/common/lib/error";
+import { IngestResult } from '../ingest'
+import ingestFactory from '../ingest'
+
+const SOURCE = 'careoregon'
+const MTAG = [ 'ops', 'data', SOURCE ]
+
+export async function ingest(context: IContext, srcKey: string): Promise<Result<IngestResult, ErrCode>> {
+  const { logger } = context;
+  const TAG = [ ...MTAG, 'ingest' ]
+
+  try {
+    const factoryResult = ingestFactory(context, SOURCE, {
+      destFilename: (srcKey.split('/').pop() as string).replace(/\.pgp$/, ''),
+    })
+
+    if (factoryResult.isErr()) {
+      logger.error(context, TAG, 'Error generating ingest function.')
+
+      return err(factoryResult.error)
+    }
+
+    const ingestFunction = factoryResult.value
+
+    const ingestResult = await ingestFunction(context, srcKey)
+
+    if (ingestResult.isErr()) {
+      logger.error(context, TAG, 'Ingest error.')
+
+      return err(ingestResult.error)
+    }
+
+    return ok(ingestResult.value)
+  }
+  catch (e) {
+    logger.exception(context, TAG, e)
+
+    return err(ErrCode.EXCEPTION)
+  }
+}
+
+export default {
+  ingest,
+}
