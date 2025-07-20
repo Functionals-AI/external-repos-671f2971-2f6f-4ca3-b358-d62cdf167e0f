df18be8d16417bebf05f99527d6d3c755dc21dcb
suducode-functionals <suducode@functionals.ai>
2025-07-20 11:33:03 -0700

    Upload file via API: mono/ops-flows/src/flows/data-ingest-hscsn.ts

---
+++ mono/ops-flows/src/flows/data-ingest-hscsn.ts
@@ -0,0 +1,57 @@
+import { DateTime } from 'luxon'
+
+import { succeed, workflow } from '@mono/common-flows/lib/builder'
+import SFTPTasks from '@mono/common-flows/lib/tasks/sftp'
+import { Hscsn } from '@mono/ops/lib/data'
+import OpsTasks from '../tasks/ingest'
+import { sftpGetConfig } from '../sftp-helper'
+
+const SOURCE = 'hscsn'
+const FLOW_NAME = `data-ingest-${SOURCE}`
+
+const MTAG = [ 'ops-flows', 'flows', FLOW_NAME ]
+
+enum State {
+  SFTPGet = 'SFTPGet',
+  Ingest = 'Ingest',
+  Success = 'Success',
+}
+
+export default workflow(function(config) {
+  if (config.isProduction) {
+    const dataBucket = config.ops_cdk?.data?.destBuckets?.externalData?.name
+
+    if (dataBucket) {
+      return {
+        cron: '0 0 * * ? *',
+        startAt: 'SFTPGet',
+        states: {
+          [State.SFTPGet]: SFTPTasks.mget({
+            sftpConfig: sftpGetConfig(SOURCE),
+            destBucket: dataBucket,
+            destPrefix: `${SOURCE}/sftp/`,
+            modifiedSince: DateTime.now().minus({ days: 60 }).toMillis(),
+            noUpdateOnMatch: true,
+            output: function(output, input) {
+              return {
+                ...input,
+                ...output,
+              }
+            },
+            next: State.Ingest,
+          }),
+          [State.Ingest]: OpsTasks.ingestFromSFTP({
+            flowName: FLOW_NAME,
+            sourceId: SOURCE,
+            ingestFunction: Hscsn.ingest,
+            next: State.Success
+          }),
+          [State.Success]: succeed(),
+        }
+      }
+    }
+    else {
+      console.log(`Source bucket required.`)
+    }
+  }
+})
