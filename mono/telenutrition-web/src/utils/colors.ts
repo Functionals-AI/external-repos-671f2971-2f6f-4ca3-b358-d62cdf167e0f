f78b419c8d04b9bc5ab82a966d3ab320547e1943
suducode-functionals <suducode@functionals.ai>
2025-07-20 11:13:24 -0700

    Upload file via API: mono/telenutrition-web/src/utils/colors.ts

---
+++ mono/telenutrition-web/src/utils/colors.ts
@@ -0,0 +1,14 @@
+export const COLORS = {
+  'f-very-dark-green': '#0C694B',
+  'f-dark-green': '#0C694B',
+  'f-light-green': '#0C694B',
+  'f-very-light-green': '0C694B',
+  'f-grey': '#515151',
+  'f-light-grey': '#E8EBEB',
+  'f-orange': '#E8754F',
+  'f-yellow': '#FFD83D',
+  'f-red': '#A2242B',
+  'f-light-red': '#A2242B94',
+  'f-blue': 'rgb(95, 145, 181)',
+  'purple-600': '#9118DC',
+};
